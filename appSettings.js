13e94b461f23a09c5ceb084fde78c2e9dded15e8
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-06-25 17:23:09 +0530

    Init

---
+++ appSettings.js
@@ -0,0 +1,11 @@
+const settings = {
+  'clientId': '4dccd1f5-dc76-4b7d-b00f-e61d6b818fe2',
+  'tenantId': 'common',
+  'graphUserScopes': [
+    'user.read',
+    'mail.read',
+    'mail.send'
+  ]
+};
+
+module.exports = settings;
